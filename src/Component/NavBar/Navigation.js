65b701bba80ca08f31f62350b0d0d0401bcff19b
Simphiwe-Sithebe <www.simz88@gmail.com>
2024-09-23 16:00:32 +0200

    round 2

--- src/Component/NavBar/Navigation.js
+++ src/Component/NavBar/Navigation.js
@@ -1,1 +1,14 @@
 import React, { Component } from 'react';
+//we can create nav bar by using the functional component or the class component
+//class component "ccc" for short cut
+class NavBar extends Component {
+    constructor(props) {
+        super(props);
+    }
+    state = {  }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default ;
