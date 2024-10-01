1c1f667c63fb30a80fd5366f74738e7b6c9d182c
Simphiwe-Sithebe <www.simz88@gmail.com>
2024-10-01 15:09:25 +0200

    Iphone products simulation

--- src/Component/DisplayContent/RightSide/RightSidePanel/Products.js
+++ src/Component/DisplayContent/RightSide/RightSidePanel/Products.js
@@ -4,16 +4,16 @@ function Products(props){
     return (
         <div className="product_main" >
             <div className="product_image">
-                <img src = "https://ik.imagekit.io/clone1010/amazon-image/mobiles/71w3oJ7aWyL._AC_UL640_FMwebp_QL65_.webp?updatedAt=1727253496110" height="300px"/>
+                <img src = {props.definition.image} height="300px"/>
             </div>
             <div className="product_name">
-                New Apple iPhone 11 (64GB) - Green
+                {props.definition.name}
             </div>
             <div className="product_rating">
-                35,678
+                {props.definition.rating}
             </div>
             <div className="product_prize">
-                50,999
+                {props.definition.price}
             </div>
         </div>
     );
--- src/Component/DisplayContent/RightSide/RightSidePanel/RightSidePanel.js
+++ src/Component/DisplayContent/RightSide/RightSidePanel/RightSidePanel.js
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./RightSide.css"
 import Products from "./Products";
 function RightSidePanel(props){
+
+    const[listOfProducts, setListOfProducts] = React.useState([]);
+
+    useEffect(() => {
+        //need an api call to retrieve data from db
+        let list = [
+        {name : "iPhone 10", price:"R21342", rating:"2.4", image:"https://ik.imagekit.io/clone1010/amazon-image/mobiles/71w3oJ7aWyL._AC_UL640_FMwebp_QL65_.webp?updatedAt=1727253496110"},
+        {name : "iPhone 11", price : "R25342", rating : "2.3", image : "https://ik.imagekit.io/clone1010/amazon-image/mobiles/71ZOtNdaZCL._AC_UL640_FMwebp_QL65_.webp?updatedAt=1727253498594"},
+        {name : "iPhone 12", price : "R26342", rating : "2.4", image : "https://ik.imagekit.io/clone1010/amazon-image/mobiles/51PuFBgBK4L._AC_UL640_FMwebp_QL65_.webp?updatedAt=1727253494281"},
+        {name : "iPhone 13", price : "R29342", rating : "2.5", image : "https://ik.imagekit.io/clone1010/amazon-image/mobiles/71i2XhHU3pL._AC_UL640_FMwebp_QL65_.webp?updatedAt=1727253494575"},
+     ]
+     setListOfProducts(list);
+    },[]);
+
     return (
         <div className="RightSide_main">
             
+            {/* <Products rating="2.4" price="21342" name="iPhone 11" image="https://ik.imagekit.io/clone1010/amazon-image/mobiles/71w3oJ7aWyL._AC_UL640_FMwebp_QL65_.webp?updatedAt=1727253496110"/>
             <Products/>
             <Products/>
+            <Products/>
+            <Products/>
+            <Products/>
+            <Products/> */}
+
+            {
+                listOfProducts.map((item) => (
+                    <Products definition ={item}/>
+
+                ))
+            }
+
         </div>
     );
 }
